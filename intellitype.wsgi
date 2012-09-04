90dba9ad9fb1193abddc62c633653574ad434e41
Lyle Scott III <lyle@digitalfoo.net>
2012-09-04 02:13:06 -0400

    Initial Commit

---
+++ intellitype.wsgi
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+#import os
+import sys
+
+#sys.path.append(os.path.dirname(__file__))
+sys.path.append('/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages')
+
+from urlparse import parse_qsl
+import xmltree
+import json
+
+
+def application(environ, start_response):
+    headers = [('Content-type', 'text/plain'),
+               ('Access-Control-Allow-Origin', '*'), 
+               ('Access-Control-Allow-Methods', 'GET'),] 
+    start_response("200 OK", headers)
+
+    xtree = xmltree.XMLTree()
+    xtree.insert_query(xtree.root, 'select * from derp')
+    xtree.insert_query(xtree.root, 'select * from yeah')
+
+    ret = ''
+
+    gets = dict(parse_qsl(environ.get('QUERY_STRING')))
+    if 'userinput' in gets:
+        userinput = gets['userinput']
+        nodes, token = xtree.get_query_parts(userinput)
+        print 'nodes', nodes
+        results = xtree.find_query(' '.join(nodes))
+
+        query = ' '.join(nodes) + ' %s'
+        results = [query % result for result in results]
+
+        print 'results', results
+        ret = json.dumps(results)
+
+    ret = ['jsonpcallback({"results": %s})' % ret,]
+
+    return ret
+
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    server = make_server('localhost', 8080, application)
+    server.serve_forever()
---
+++ xmltree.py
@@ -0,0 +1,113 @@
+from lxml import etree
+
+
+class XMLTree(object):
+
+    def __init__(self):
+        """ """
+        self.root = etree.Element('querytree')
+        self.tree = etree.ElementTree(self.root)
+
+    def __repr__(self):
+        """ """
+        return etree.tostring(self.root, pretty_print=1)
+
+    def tokenize(self, query):
+        """ """
+        query = query.replace('*', '__ALL__')
+        query = query.split()
+
+        return query
+
+    def _get_xpath(self, tokens):
+        """ """
+        return '//%s' % '/'.join(tokens)
+
+    def insert_query(self, root, query):
+        """ """
+        tokens = self.tokenize(query)
+        prevtoken = root
+        for i in range(len(tokens)):   
+            subtokens = tokens[:i+1]
+            subtokens_xpath = self._get_xpath(subtokens)
+            existing_path = root.xpath(subtokens_xpath)
+            if not existing_path:
+                prevtoken = etree.SubElement(prevtoken, tokens[i])
+            else:
+                prevtoken = existing_path[0]
+
+    def find_query(self, query):
+        """ """
+        tokens = self.tokenize(query)
+        xpath = self._get_xpath(tokens)
+
+        try:
+            node = self.root.xpath(xpath)
+        except etree.XPathEvalError:
+            return []
+
+        if node:
+            if len(node) != 1:
+                print 'ERROR'
+            ret = []
+            children = node[0].getchildren()
+            for child in children:
+                ret.append(child.tag)
+
+            return ret
+
+        return []
+                
+    def get_query_parts(self, query):
+        """ """
+        blank_at_end = query[-1] == ' '
+        query = query.split()
+        if blank_at_end:
+            nodes = query
+            token = ''
+        else:
+            nodes = query[:-1]
+            token = query[-1]
+
+        return (nodes, token,)
+
+    def get_leaf_nodes(self, node, leaves=None):
+        """Return all the leaf nodes in the tree."""
+        if leaves is None:
+            leaves = []
+
+        children = node.getchildren()
+
+        if not children:
+            leaves.append(node)
+            return
+
+        for child in children:
+            self.get_leaf_nodes(child, leaves)
+
+        return leaves
+
+    def get_leaf_paths(self, node, paths=None):
+        """Return the paths of all leaf nodes."""
+        if paths is None:
+            paths = []
+
+        children = node.getchildren()
+
+        if not children:
+            paths.append(self.tree.getpath(node))
+            return
+
+        for child in children:
+            self.get_leaf_paths(child, paths)
+
+        return paths
+
+
+if __name__ == '__main__':
+    xmltree = XMLTree()
+    xmltree.insert_query(xmltree.root, 'select * from derp')
+    xmltree.insert_query(xmltree.root, 'select * from yeah')
+
+    print xmltree.get_leaf_nodes(xmltree.root)
+    print xmltree.get_leaf_paths(xmltree.root)
